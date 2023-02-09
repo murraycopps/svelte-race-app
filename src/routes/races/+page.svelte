28592aa7fdab7812d9d3caf10f361129280792c5
murraycopps <murraycopps@gmail.com>
2023-02-08 20:44:43 -0500

    made cards components

--- src/routes/races/+page.svelte
+++ src/routes/races/+page.svelte
@@ -1,18 +1,21 @@
-<script>
+<script lang="ts">
+	import RunCard from './RunCard.svelte';
 	import { races } from '$lib/scripts/races';
 
 	let selectedRaces = races;
 
-	const changeDistance = (/** @type {any} */ e) => {
-		// set selected races to races.filter
-		if (!e?.target.value) return;
+	
 
-		if (e.target.value === 'all') {
+	const changeDistance = (e: {currentTarget: HTMLSelectElement}) => {
+		// set selected races to races.filter
+		if (!e?.currentTarget?.value) return;
+
+		if (e.currentTarget.value === 'all') {
 			selectedRaces = races;
 			return;
 		}
 
-		if (e.target.value === 'marathon') {
+		if (e.currentTarget.value === 'marathon') {
 			selectedRaces = races.filter(
 				(race) =>
 					race.distance.toLowerCase().includes('marathon') &&
@@ -21,7 +24,7 @@
 			return;
 		}
 
-		selectedRaces = races.filter((race) => race.distance.toLowerCase().includes(e.target.value));
+		selectedRaces = races.filter((race) => race.distance.toLowerCase().includes(e.currentTarget.value));
 	};
 </script>
 
@@ -44,20 +47,7 @@
 		</select>
 		<ul>
 			{#each selectedRaces as race}
-				<li>
-					<a href="/races/{race.id}">
-						<h2>{race.name}</h2>
-						<span class="details">
-							{#if !race.distance.toLowerCase().includes('track')}
-								<p>{race.distance}</p>
-							{:else}
-								<p>Track Meet</p>
-							{/if}
-							<p>{race.location}</p>
-							<p>{race.date}</p>
-						</span>
-					</a>
-				</li>
+				<RunCard race={race} />
 			{/each}
 		</ul>
 	{:else}
@@ -80,58 +70,20 @@
 		width: clamp(500px, 50vw, 800px);
 		max-width: 100%;
 	}
-	p {
-		margin: 0;
-	}
-	li {
-		margin: 1.5rem 0;
-		/* background-color: #1f2937; */
-		background-color: var(--off-white);
-		padding: 1.5rem;
-		/* border: 1px solid var(--bright-orange); */
-		border: 2px solid var(--dark);
-		border-radius: 0.5rem;
-		box-shadow: 0 0 0.25rem 0.25rem var(--dark);
-		/* box-shadow: 0 0 0.25rem 0.25rem var(--bright-orange); */
-	}
-	a {
-		color: black;
-		text-decoration: none;
-		font-weight: bold;
-	}
-
+    
 	h1 {
-		font-size: 4rem;
+        font-size: 4rem;
 		font-weight: 400;
 		margin: 0;
 	}
-	.details {
-		--slant-amount: 2.5rem;
-		background-color: var(--dark);
-		/* background-color: var(--bright-orange); */
-		padding: 1rem var(--slant-amount);
-		display: flex;
-		justify-content: space-evenly;
-		align-items: center;
-		width: 100%;
-		clip-path: polygon(
-			var(--slant-amount) 0%,
-			100% 0%,
-			calc(100% - var(--slant-amount)) 100%,
-			0% 100%
-		);
-		color: var(--off-white);
-		flex-wrap: wrap;
-	}
-
 	select {
 		text-indent: 0.5rem;
 		border-radius: 0.5rem;
 		background-color: var(--dark);
 		font-size: 1.5rem;
 		color: var(--off-white);
-        height: 2.5rem;
-        width: clamp(200px, 25vw, 300px);
+		height: 2.5rem;
+		width: clamp(200px, 25vw, 300px);
 	}
 
 	option {
---
+++ src/routes/races/RunCard.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import type { Race } from '$lib/scripts/types';
+
+	export let race: Race;
+</script>
+
+<li>
+	<a href="/races/{race.id}">
+		<h2>{race.name}</h2>
+		<span class="details">
+			{#if !race.distance.toLowerCase().includes('track')}
+				<p>{race.distance}</p>
+			{:else}
+				<p>Track Meet</p>
+			{/if}
+			<p>{race.location}</p>
+			<p>{race.date}</p>
+		</span>
+	</a>
+</li>
+
+<style>
+	p {
+		margin: 0;
+	}
+	li {
+		margin: 1.5rem 0;
+		/* background-color: #1f2937; */
+		background-color: var(--off-white);
+		padding: 1.5rem;
+		/* border: 1px solid var(--bright-orange); */
+		border: 2px solid var(--dark);
+		border-radius: 0.5rem;
+		box-shadow: 0 0 0.25rem 0.25rem var(--dark);
+		/* box-shadow: 0 0 0.25rem 0.25rem var(--bright-orange); */
+	}
+	a {
+		color: black;
+		text-decoration: none;
+		font-weight: bold;
+	}
+	.details {
+		--slant-amount: 2.5rem;
+		background-color: var(--dark);
+		/* background-color: var(--bright-orange); */
+		padding: 1rem var(--slant-amount);
+		display: flex;
+		justify-content: space-evenly;
+		align-items: center;
+		width: 100%;
+		clip-path: polygon(
+			var(--slant-amount) 0%,
+			100% 0%,
+			calc(100% - var(--slant-amount)) 100%,
+			0% 100%
+		);
+		color: var(--off-white);
+		flex-wrap: wrap;
+	}
+</style>
